a3f2bd21d00e16f1861987729e48d682ba2df42d
Guilhem Fauré <pro@guilhemfau.re>
2026-03-10 10:11:13 +0100

    finish heure

--- tp1/ex4/p1/Heure.java
+++ tp1/ex4/p1/Heure.java
@@ -31,23 +31,28 @@ class Heure {
 
   public void setHeures(int h) {
     if (h < 0 || h > 23)
-      throw new IllegalArgumentException("Heure entre 0 et 23");
+      throw new IllegalArgumentException("Heure doit être entre 0 et 23!");
     this.h = (byte) h;
   }
 
   public void setMinutes(int m) {
     if (m < 0 || m > 59)
-      throw new IllegalArgumentException("Minutes entre 0 et 59");
+      throw new IllegalArgumentException("Minutes doit être entre 0 et 59!");
     this.m = (byte) m;
   }
 
   public void setHeuresMinutes(int h, int m) {
     if (h < 0 || h > 23)
-      throw new IllegalArgumentException("Heure entre 0 et 23");
+      throw new IllegalArgumentException("Heure doit être entre 0 et 23");
     if (m < 0 || m > 59)
-      throw new IllegalArgumentException("Minutes entre 0 et 59");
+      throw new IllegalArgumentException("Minutes doit être entre 0 et 59!");
     this.h = (byte) h;
     this.m = (byte) m;
+  }
+
+  public void avancerMinutes(int m) {
+    this.h = (byte) ((this.h + (this.m + m) / 60) % 24);
+    this.m = (byte) ((this.m + m) % 60);
   }
 
   public void main() {
@@ -59,6 +64,15 @@ class Heure {
     } catch (IllegalArgumentException e) {
       System.out.println("Erreur: " + e.getMessage());
     }
+    System.out.println(h);
+    h.setHeuresMinutes(23, 59);
+    System.out.println(h);
+    h.avancerMinutes(1);
+    System.out.println(h);
+    h.avancerMinutes(999);
+    System.out.println(h);
+    h.avancerMinutes(1235134624);
+    System.out.println(h);
     System.out.println(h);
   }
 }
