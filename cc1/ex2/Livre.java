a62bee7f7cc27cf63d6da70238611a0510cf3994
Guilhem Fauré <pro@guilhemfau.re>
2026-03-18 11:56:28 +0100

    ex6

--- cc1/ex2/Livre.java
+++ cc1/ex2/Livre.java
@@ -4,9 +4,19 @@ import cc1.ex1.GenreLitteraire;
 
 public class Livre {
   private String titre;
-  private String auteur;
   private long isbn;
   private GenreLitteraire genre;
+  // QUESTION 6
+  // Pour que l’auteur d’un livre soit représenté par une classe Auteur,
+  // il faudrait commencer par la créer, dans un fichier distinct Auteur.java
+  //
+  // Pour obtenir le même niveau de fonctionnalité qu’actuellement, il faudrait à
+  // minima que cette classe dispose d’un attribut `String nom` et d’une
+  // redéfinition de toString() qui le retourne.
+  //
+  // Il faudrait aussi évidemment que cette classe dispose d’un contructeur pour
+  // pouvoir créer des auteurs avec leurs informations.
+  private String auteur;
 
   private long CheckIsbn(long isbn) {
     if (isbn < 9780000000000L && isbn > 9799999999999L)
@@ -14,12 +24,8 @@ public class Livre {
     return isbn;
   }
 
-  // public Livre() {
-  // this.titre = "Titre";
-  // this.auteur = "Auteur";
-  // this.isbn = 9780000000000L;
-  // this.genre = GenreLitteraire.DEFAULT;
-  // }
+  public Livre() {
+  }
 
   public Livre(String titre, String auteur, long isbn, GenreLitteraire genre) throws IllegalArgumentException {
     this.titre = titre;
